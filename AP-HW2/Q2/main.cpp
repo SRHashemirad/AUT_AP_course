71fc095ac0a22c44d551189ca9ed6581dfb081e3
SRHashemirad <SR.Hashemirad@yahoo.com>
2019-03-06 12:27:23 +0330

    Showing results updated

--- AP-HW2/Q2/main.cpp
+++ AP-HW2/Q2/main.cpp
@@ -20,22 +20,25 @@ int main() {
 	pointer_to_arr_counter ptrArrCount = &LibArr::counter;
 
 	std::cout << "The run times are in micro secondes!" << std::endl;
-	std::cout << std::setw(9) << "   Size  " << " | "
-			  << std::setw(9) << "  Vector " << " | "
-			  << std::setw(9) << "  Array  "
+	std::cout << std::setw(11) << "    Size   " << " | "
+			  << std::setw(11) << "   Vector  " << " | "
+			  << std::setw(11) << "   Array   " << " | "
+			  << std::setw(12) << "   Sum     "
 			  << std::endl
-			  << std::setw(36) << "------------------------------------"
+			  << std::setw(44) << "------------------------------------------------------- "
 			  << std::endl;
 	
 	for (long long i{1LL}; i <= 1000000LL; i *= 10LL)
 	{
-		std::cout << std::setw(9) << i << " | "
-			  << std::setw(9) << runTime(vec, ptrVecCount, i) << " | "
-			  << std::setw(9) << runTime(arr, ptrArrCount, i)
-			  << std::endl
-			  << std::setw(36) << "------------------------------------"
-			  << std::endl;
+		std::cout << std::setw(11) << i << " | "
+				  << std::setw(11) << runTime(vec, ptrVecCount, i) << " | "
+				  << std::setw(11) << runTime(arr, ptrArrCount, i) << " | "
+				  << std::setw(12) << vec.counter(i)
+				  << std::endl
+				  << std::setw(44) << "------------------------------------------------------- "
+				  << std::endl;
 	}
+
 
 	return 0;
 }
@@ -44,7 +47,8 @@ int main() {
 template<typename T1, typename T2>
 long long runTime(T1 obj, T2 ptrCounter, long long n) {
 	long int start_s{clock()};
+	//std::cout << "S: " <<start_s << std::endl;
 	(obj.*ptrCounter)(n);
 	long int stop_s{clock()};	
-	return (stop_s - start_s) / static_cast<double>(CLOCKS_PER_SEC) * 1000000;
+	return (stop_s - start_s) / static_cast<long double>(CLOCKS_PER_SEC) * 1000000;
 }
