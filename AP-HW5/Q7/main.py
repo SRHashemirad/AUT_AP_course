0590ee193e001f5b8aaed6b5c525abb38c13f10c
Unknown <sr.hashemirad@yahoo.com>
2019-05-30 08:19:18 +0430

    Q7 was done and codes added!

---
+++ AP-HW5/Q7/main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 30 06:35:55 2019
+
+@author: SeyedReza
+"""
+#version ONE
+#x = input().split(' ')
+#print(' '.join([num for num in [x[i] for i in range(len(x)) if (not (i+1)%6)] if (not int(num)%6)]))
+
+
+#version TWO
+#x = input().split(' ')
+#print(' '.join([x[i] for i in range(len(x)) if (not int(x[i])%6) and (not (i+1)%6)]))
+
+
+#test1: 3 4 1 37 21 18 23 21 27 22 43 21
+#test2: 1 2 3 4 5 6 7 8 9 10 11 12
